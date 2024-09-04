b9d0408737ca8817139d07d8fe15d67a9b72f9d4
Varsha <bharambevarsha13@gmail.com>
2024-09-04 15:58:42 +0530

    initial commit

---
+++ src/main/java/com/electronics/ElectronicStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.electronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicStoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electronics/ElectronicStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicStoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
